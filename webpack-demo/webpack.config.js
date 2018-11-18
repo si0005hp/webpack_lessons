8c487821f23f3ac294809be24895e4954b55ef50
si0005hp <si0005hp@gmail.com>
2018-11-19 01:31:24 +0800

    Official Asset Management css

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -3,7 +3,18 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // any file that ends with .css
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
