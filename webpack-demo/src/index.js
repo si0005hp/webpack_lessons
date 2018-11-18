496530fe64f0435ae1fcf702ac9e4ea1a7fb3897
si0005hp <si0005hp@gmail.com>
2018-11-19 01:45:02 +0800

    Official Asset Management file

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
   let element = document.createElement('div');
@@ -7,6 +8,11 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
